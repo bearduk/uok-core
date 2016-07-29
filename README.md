7b18cea929bf355d9e7f7613ca4aa3e29f8cb0ce
Chris Beard <coebe500@aol.com>
2016-07-29 15:18:56 +0100

    Populated project with old CodeKit SASS. Gruntfile configured with grunt-contrib-compass. README.md updated to reflect requirements for build. Gemfile added. .gitignore added.

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# uok-core
+# Keele University - uok-core
 ## University of Keele core files
 SASS and JS repo of core CMS styles.
 
 Based on Node (including npm) and Grunt.
 
 This repo is a rebase of the old CodeKit setup.
+
+You will need to have NodeJS (including NPM), Git and Ruby (including RubyGems) installed on your machine.
+
+## What you'll need
+You will need to have NodeJS (including NPM), Git, Ruby (including RubyGems) and Compass installed on your machine.
+
+Gem bundler from <http://bundler.io/>
--- gruntfile.js
+++ gruntfile.js
@@ -6,10 +6,30 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		// create link to package.json items
     	pkg: grunt.file.readJSON('package.json'),
-	
+		
+
+    	compass: {                  	// Task
+    		dist: {                   	// Target
+				options: {              // Target options
+			    	sassDir: 'components/sass',
+			    	cssDir: 'builds/development/css',
+			    	environment: 'development'
+      			}
+    		}, // close dist
+    		dev: {                    // Another target
+      			options: {
+        			sassDir: 'components/sass',
+        			cssDir: 'builds/production'
+      			}
+    		} // close dev
+    	}
 
 	}); //initConfig
 
-	grunt.registerTask('default', []);
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	// grunt.loadNpmTasks('grunt-contrib-sass');
+	
+
+	grunt.registerTask('default', ['compass']);
 	
 }; // wrapper function
